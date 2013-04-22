6b9590401d02a9f05618cded303c5195a06ed2b9
Youssef HARRATI <harrati.youssef@gmail.com>
2013-04-22 08:53:24 +0000

    BIController

---
+++ PFE_etat_civil2-war/src/java/controller/BIController.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "bIController")
+@SessionScoped
+public class BIController implements Serializable {
+
+    @EJB
+    private session.Acte_DecesFacade acte_DecesFacade;
+    @EJB
+    private session.Acte_NaissanceFacade acte_NaissanceFacade;
+    private Integer anneeGeneral = Integer.parseInt(new SimpleDateFormat("yyyy").format(new Date()));
+    private List<Integer> data = new ArrayList<Integer>();
+
+    public Integer getAnneeGeneral() {
+        return anneeGeneral;
+    }
+    
+    public void setAnneeGeneral(Integer anneeGeneral) {
+        this.anneeGeneral = anneeGeneral;
+    }
+    
+    public List<Integer> getData() {
+        data = new ArrayList<Integer>();
+        data.add((int)(Math.random()*80));
+        data.add((int)(Math.random()*80));
+        data.add((int)(Math.random()*80));
+        data.add((int)(Math.random()*80));
+        data.add((int)(Math.random()*80));
+        data.add((int)(Math.random()*80));
+        data.add((int)(Math.random()*80));
+        data.add((int)(Math.random()*80));
+        data.add((int)(Math.random()*80));
+        data.add((int)(Math.random()*80));
+        data.add((int)(Math.random()*80));
+        data.add((int)(Math.random()*80));
+        return data;
+    }
+
+    public void setData(List<Integer> data) {
+        this.data = data;
+    }
+    
+}
